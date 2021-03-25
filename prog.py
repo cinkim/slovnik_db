27fce98ee4127fb9be022c499814c5137883ed96
Petr Fišer <cinkim@seznam.cz>
2021-03-25 13:53:09 +0100

    novy uzivatel napojeny na DB

--- prog.py
+++ prog.py
@@ -2,27 +2,22 @@
 
 import tkinter as tk
 from tkinter import ttk, StringVar, NORMAL, CENTER, N, S, E, W
-from tkinter import LEFT, NO, DISABLED, NORMAL
+from tkinter import LEFT, NO, DISABLED, NORMAL, YES
 import tkinter.messagebox
 
 import new_student as ns
+import prace_s_db
 
 
 class slovnik:
 
     def __init__(self):
-        self.seznam_studentu = self.nacti_studenty()
+        self.seznam_studentu = []
+        self.nacti_studenty()
 
 
     def nacti_studenty(self):
-        studenti = []
-        with open("studenti.txt", mode="r", encoding="utf-8") as seznam_st:
-            for student in seznam_st:
-                student = student.strip()
-                studenti.append(student)
-            return studenti
-
-
+        self.seznam_studentu = prace_s_db.seznam_studentu()
 
 class slovnikGUI(tk.Frame):
 
@@ -33,20 +28,23 @@ class slovnikGUI(tk.Frame):
         self.parent.title("Slovnik")
         self.parent.protocol("WM_DELETE_WINDOW", self.on_close)
         self.create_widgets()
+        self.zobraz()
 
 
     def create_widgets(self):
         self.kdo = tk.LabelFrame(root, text="Kdo jsi", font="Arial 8")
         self.kdo.grid(row=1, column=0, sticky=W)
-        if len(self.slovnik.seznam_studentu) == 0:
-            pass
-        else:
-            self.aktualni_osoba = StringVar()
-            self.aktualni_osoba.set(self.slovnik.seznam_studentu[0])
 
-            for self.uziv in self.slovnik.seznam_studentu:
-                self.RB = tk.Radiobutton(self.kdo, text=self.uziv, variable=self.aktualni_osoba, value=self.uziv, width=20, anchor=W, command=self.nacti_ucivo)
-                self.RB.grid()
+        self.tree_zaznamy = ttk.Treeview(self.kdo, column=("student"), height=8)
+        self.tree_zaznamy['show'] = 'headings' # schová první sloupec s identifikátorem
+        self.tree_zaznamy.grid(row=2, column=0)
+
+        
+        self.tree_zaznamy.heading("#0", text="#")
+        self.tree_zaznamy.column("#0", width=0, stretch=NO, anchor='center')
+
+        self.tree_zaznamy.heading("student", text="Student")
+        self.tree_zaznamy.column("student", minwidth=0, width=124, stretch=NO, anchor='center')
 
         self.button_Konec = tk.Button(root, text="Nový student", command=self.novy_student, fg="blue", font="Arial 8", width=20)
         self.button_Konec.grid(row=9, column=0, sticky=W)
@@ -55,24 +53,29 @@ class slovnikGUI(tk.Frame):
         self.button_Konec.grid(row=10, column=0, sticky=W)
         
 
-
+    def novy(self):
+        self.novy = ns.ulozit_noveho_studenta(self)
+        prace_s_db.pridat_studenta(self.novy)
+        self.slovnik.nacti_studenty()
+        self.zobraz()
+        return
 
 
     def novy_student(self):
+        """
+        Otevře okno pro registraci studenta
+        """
         ns.zaloz_studenta(self)
 
-    def novy(self):
-        ns.ulozit_noveho_studenta(self)
-        self.slovnik.__init__()
-        self.__init__(parent, slovnik)
-        root.update()
 
+    def zobraz(self):
+        for ii in self.tree_zaznamy.get_children():
+            self.tree_zaznamy.delete(ii)
 
-
-    def nacti_ucivo(self):
-        a=self.aktualni_osoba.get()
-        print(a)
-
+        pozice = 0
+        for zaznam in self.slovnik.seznam_studentu:
+            self.tree_zaznamy.insert("", "end", text=pozice, values=zaznam)
+            pozice += 1
 
     # zavřít
     def on_close(self):
