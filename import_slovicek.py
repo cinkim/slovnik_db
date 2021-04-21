5834c80519ee3f0d36442767cccc32a7869e49ea
Petr Fišer <cinkim@seznam.cz>
2021-04-21 10:57:18 +0200

    pridan file dialog a messagebox pro nepodporovane soubory

--- import_slovicek.py
+++ import_slovicek.py
@@ -1,29 +1,34 @@
+import tkinter as tk
+from tkinter import filedialog
+import tkinter.messagebox
+
 import pandas as pd
 import pathlib
 from prace_s_db import pripojeni_db
 
-soubor = 'test_excel.xlsx'
 
-def import_slovicek(lekce_id, soubor):
+def import_sl(self):
+    file_path = filedialog.askopenfilename()
+    print(file_path)
+
     try:
-        soubor_typ = pathlib.Path(soubor).suffix
-        
+        soubor_typ = pathlib.Path(file_path).suffix       
         if "xl" in soubor_typ:
-            seznam_slov = pd.read_excel(soubor, engine = 'openpyxl')
+            seznam_slov = pd.read_excel(file_path, engine = 'openpyxl')
 
         elif "csv" or "txt" in soubor_typ:
-            seznam_slov = pd.read_csv(soubor, delimiter=';', usecols=(0,1))
-            
-
-        seznam_slov['lekce_id'] = lekce_id
-
-        conn, cursor = pripojeni_db()
-        seznam_slov.to_sql("SLOVICKA", conn, if_exists='append', index=False)
-        
-
+            seznam_slov = pd.read_csv(file_path, delimiter=';', usecols=(0,1))
+    
     except ValueError:
-            print("Nepodporovaný formát. Podporované formáty jsou xls,xlsx,csv,txt")
+        tk.messagebox.showwarning("ERROR", "Nepodporovaný typ souboru\naplikace podporuje pouze soubory typu\ncsv, txt, xls")
+        return
+    print("haha")
+    """
+    seznam_slov['lekce_id'] = lekce_id
 
+    conn, cursor = pripojeni_db()
+    seznam_slov.to_sql("SLOVICKA", conn, if_exists='append', index=False)
+    """
 
 #import_slovicek(99,soubor)
 
